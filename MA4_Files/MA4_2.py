e3a5abf8445836d0dbac829d5c4f4eb634518da6
Axel Wohlin <axwo0926@arrhenius.it.uu.se>
2022-05-16 12:13:18 +0200

    La till fib funktionen till python och numba

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -7,6 +7,16 @@ def main():
 	print(f.get())
 	ans = 1 +2 
 	return ans
-
+def fib_py(n):
+	if n<= 1:
+		return n
+	else:
+		return(fib_py(n-1)+fib_py(n-2))
+@njit
+def fib_numba(n):
+	if n<= 1:
+		return n
+	else:
+		return(fib_numba(n-1)+fib_numba(n-2))
 if __name__ == '__main__':
 	main()
