641387e4f320dbe1e2c9af25bd8f3efa160b9e63
Axel Wohlin <axwo0926@arrhenius.it.uu.se>
2022-05-18 13:06:17 +0200

    Färdig på linuxmaskin, redovisa på lokal

--- MA4_Files/MA4_2.py
+++ MA4_Files/MA4_2.py
@@ -1,12 +1,29 @@
 from person import Person
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+from numba import njit
+
 
 def main():
 	f = Person(5)
 	print(f.get())
 	f.set(7)
 	print(f.get())
-	ans = 1 +2 
-	return ans
+	
+	start = pc()
+	print(	fib_py(35) )
+	end = pc()
+	print(f"Regular python took {end-start} seconds")
+	start2 = pc()
+	print( fib_numba(35) )
+	end2 = pc()
+	print(f"Numba python took {end2-start2} seconds")
+	start3 = pc()
+	p = Person(35)
+	print(p.fib())
+	end3 = pc()
+	print(f"C++ took {end3-start2} seconds")
+
 def fib_py(n):
 	if n<= 1:
 		return n
--- MA4_Files/person.cpp
+++ MA4_Files/person.cpp
@@ -6,18 +6,36 @@ class Person{
 		Person(int);
 		int get();
 		void set(int);
+		int fib();
+		int fib2(int);
 	private:
+
 		int age;
-	};
- 
+		int fib2(int);
 Person::Person(int n){
 	age = n;
-	}
- 
+}
+int Person::fib(){
+
+	if(age<=1){
+		return age;
+			}
+	else{
+	     	return (this->fib2(age-1) + this->fib2(age-2) );
+		}
+}
+
 int Person::get(){
 	return age;
 	}
- 
+int Person::fib2(int n){
+	if(n<=1){
+                return n;
+                        }
+        else{
+                return (fib2(n-1) + fib2(n-2) );
+                }
+	}
 void Person::set(int n){
 	age = n;
 	}
@@ -26,6 +44,7 @@ void Person::set(int n){
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
+	int Person_fib(Person* person) {return person->fib();}
 	void Person_set(Person* person, int n) {person->set(n);}
 	void Person_delete(Person* person){
 		if (person){
